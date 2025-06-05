b9c671a57dab6d39e3115a8f117b303ec463fe55
Jennie Long <longjen100@gmail.com>
2025-06-05 09:43:46 -0700

    moved tests out of src

---
+++ tests/app.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn render() {
+        let app = App::default();
+        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
+
+        app.render(buf.area, &mut buf);
+
+        // TOOD: test the rendered app
+    }
+}
