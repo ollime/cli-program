f89ec851087091f9bfd95d80ef74723a62917d30
Jennie Long <longjen100@gmail.com>
2025-06-05 18:17:27 -0700

    implemented double nested blocks

--- src/ui.rs
+++ src/ui.rs
@@ -34,10 +34,17 @@ impl Widget for &App {
             ]
         );
 
-        let block = Block::bordered()
+        // Renders double nested blocks
+        let outer_block = Block::bordered()
             .border_set(border::ROUNDED)
-            .padding(Padding::new(5, 5, 5, 5))
+            .padding(Padding::horizontal(1))
             .title(title);
+        let inner_block = Block::bordered()
+            .border_set(border::ROUNDED)
+            .padding(Padding::new(5, 5, 5, 5));
+        let outer_area = area;
+        let inner_area = outer_block.inner(outer_area); // gets area inside of outer_block
+        inner_block.render(inner_area, buf); // renders inner_area in outer_block
 
         // let paragraph = Paragraph::new(content)
         //     .block(block)
@@ -45,7 +52,7 @@ impl Widget for &App {
         //     .centered();
 
         let tabs = Tabs::new(vec!["tab1".red(), "tab2".blue(), "tab3".yellow()])
-            .block(block)
+            .block(outer_block)
             .select(2)
             .divider(symbols::DOT)
             .padding("->", "<-");
@@ -53,3 +60,39 @@ impl Widget for &App {
         tabs.render(area, buf);
     }
 }
+
+// 124fn render_email(selected_index: usize, area: Rect, buf: &mut Buffer) {
+// 125    let theme = THEME.email;
+// 126    let email = EMAILS.get(selected_index);
+// 127    let block = Block::new()
+// 128        .style(theme.body)
+// 129        .padding(Padding::new(2, 2, 0, 0))
+// 130        .borders(Borders::TOP)
+// 131        .border_type(BorderType::Thick);
+// 132    let inner = block.inner(area);
+// 133    block.render(area, buf);
+// 134    if let Some(email) = email {
+// 135        let vertical = Layout::vertical([Constraint::Length(3), Constraint::Min(0)]);
+// 136        let [headers_area, body_area] = vertical.areas(inner);
+// 137        let headers = vec![
+// 138            Line::from(vec![
+// 139                "From: ".set_style(theme.header),
+// 140                email.from.set_style(theme.header_value),
+// 141            ]),
+// 142            Line::from(vec![
+// 143                "Subject: ".set_style(theme.header),
+// 144                email.subject.set_style(theme.header_value),
+// 145            ]),
+// 146            "-".repeat(inner.width as usize).dim().into(),
+// 147        ];
+// 148        Paragraph::new(headers)
+// 149            .style(theme.body)
+// 150            .render(headers_area, buf);
+// 151        let body = email.body.lines().map(Line::from).collect_vec();
+// 152        Paragraph::new(body)
+// 153            .style(theme.body)
+// 154            .render(body_area, buf);
+// 155    } else {
+// 156        Paragraph::new("No email selected").render(inner, buf);
+// 157    }
+// 158}
