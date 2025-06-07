8b6c99642a47570826ba8f7df72ae8ddc3684fcd
Jennie Long <longjen100@gmail.com>
2025-06-06 23:01:21 -0700

    added page content to each tab

--- src/ui.rs
+++ src/ui.rs
@@ -44,21 +44,31 @@ impl Widget for &App {
         let inner_block = Block::bordered()
             .border_set(border::ROUNDED)
             .padding(Padding::new(5, 5, 5, 5));
-        let outer_area = area;
         // By defining inner_area inside of outer_block, the rendered inner_block
         // will be inside of outer_block.
-        let inner_area = outer_block.inner(outer_area); // gets area inside of outer_block
+        let inner_area = outer_block.inner(area); // gets area inside of outer_block
         
         // Renders text inside of inner_block
-        let paragraph = Paragraph::new(Text::from(self.input_value.as_str()))
-            .wrap(Wrap {trim: true})
-            .centered();
+        // let paragraph = Paragraph::new(Text::from(self.input_value.as_str()))
+        //     .wrap(Wrap {trim: true})
+        //     .centered();
         let paragraph_area = inner_block.inner(inner_area);
 
         inner_block.render(inner_area, buf); // renders inner_block in outer_block
-        paragraph.render(paragraph_area, buf); // renders paragraph in inner_block
+        // paragraph.render(paragraph_area, buf); // renders paragraph in inner_block
 
         self.render_tabs(area, buf);
+        self.current_screen.render(paragraph_area, buf);
+    }
+}
+
+impl Widget for CurrentScreen {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        match self {
+            Self::Tab1 => self.render_tab(area, buf, String::from("first page")),
+            Self::Tab2 => self.render_tab(area, buf, String::from("test")),
+            Self::Tab3 => self.render_tab(area, buf, String::from("!!!")),
+        }
     }
 }
 
@@ -66,18 +76,24 @@ impl CurrentScreen {
     fn title(self) -> Line<'static> {
         format!(" {self} ")
             .into()
-}
+    }
+
+    // content for each tab
+    fn render_tab(self, area: Rect, buf: &mut Buffer, text: String) {
+        Paragraph::new(text)
+            .block(Block::new())
+            .render(area, buf);
+    }
 }
 
 impl App {
     fn render_tabs(&self, area: Rect, buf: &mut Buffer) {
         let tab_titles = CurrentScreen::iter().map(CurrentScreen::title);
         let current_screen_index = self.current_screen as usize;
-        let tabs = Tabs::new(tab_titles)
+        Tabs::new(tab_titles)
             .select(current_screen_index) // sets tab index
             .divider(symbols::DOT)
-            .padding("->", "<-");
-
-        tabs.render(area, buf);
+            .padding("->", "<-")
+            .render(area, buf);
     }
 }
