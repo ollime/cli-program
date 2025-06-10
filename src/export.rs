eb45dc7be85f5ff8e6b7746cc5a46e531abd7a8f
Jennie Long <longjen100@gmail.com>
2025-06-10 08:41:43 -0700

    added white border & multiline viewing to exported html

--- src/export.rs
+++ src/export.rs
@@ -24,7 +24,9 @@ impl Export {
             Ok(file) => file,
         };
 
+        // println!("{:?}", input_value.split("\n").collect::<Vec<_>>());
         let html_text = Export::format_html(&file_name, input_value);
+        println!("{}", html_text);
 
         // inserts input_value into the file
         match file.write_all(&html_text.as_bytes()) {
@@ -35,27 +37,64 @@ impl Export {
     }
 
     fn format_html(file_name: &str, input_value: &str) -> String {
+        let text_content: Vec<_> = input_value.split('\n')
+            .map(|line| {
+                if line.len() > 0 {
+                    format!("\t\t\t<p>{}</p>", line)
+                }
+                else {
+                    String::from("\t\t\t<br>")
+                }
+            })
+            .collect();
+        // creates a new line for html formatting purposes only
+        let text_content = text_content.join("\n"); // no visual effect on page
+
         let css_styles = "
             html {
                 background-color: black;
                 color: white;
+                font: 1em 'arial';
+            }
+            html,
+            body {
+                display: flex;
+                margin: 0;
+                padding: 0;
+                height: 100%;
+                width: 100%;
+            }
+            .container {
+                flex: 1;
+                border: 2px solid white;
+                padding: 20px;
+                box-sizing: border-box;
+                margin: 20px;
+            }
+            h1 {
+                font-size: 1.5em;
             }
         ";
         
-        // TODO: retain newlines in html file
+        // keep alignment/tabbing like this to ensure resulting HTML file is formatted correctly
         let html_text = format!(
 "<!DOCTYPE html>
 <html lang=\"en\">
-    <head>
-        <meta charset=\"UTF-8\" />
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
-        <title>{}</title>
-        <style>{}</style>
-    </head>
-    <body>
+  <head>
+    <meta charset=\"UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <title>{}</title>
+    <style>
         {}
-    </body>
-</html>", file_name, css_styles, input_value);
+    </style>
+  </head>
+  <body>
+    <div class=\"container\">
+      <h1>{}</h1>
+{}
+    </div>
+  </body>
+</html>", file_name, css_styles, file_name, text_content);
 
         html_text
     }
